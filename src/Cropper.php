a9aaae2d883a874e85e19cb1789974a5828ca61e
Ercan Bilgin <bilginnet@hotmail.com>
2017-08-26 14:20:55 +0300

    Update Cropper.php

--- src/Cropper.php
+++ src/Cropper.php
@@ -100,6 +100,16 @@ class Cropper extends InputWidget
         if (!isset($options['icons']['browse'])) $options['icons']['browse'] = '<i class="fa fa-image"></i>';
         if (!isset($options['icons']['crop'])) $options['icons']['crop'] = '<i class="fa fa-crop"></i>';
         if (!isset($options['icons']['close'])) $options['icons']['close'] = '<i class="fa fa-crop"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['zoom-in'] = '<i class="fa fa-search-plus"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['zoom-out'] = '<i class="fa fa-search-minus"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['rotate-left'] = '<i class="fa fa-rotate-left"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['rotate-right'] = '<i class="fa fa-rotate-right"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['flip-horizontal'] = '<i class="fa fa-arrows-h"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['flip-vertical'] = '<i class="fa fa-arrows-v"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['move-left'] = '<i class="fa fa-arrow-left"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['move-right'] = '<i class="fa fa-arrow-right"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['move-up'] = '<i class="fa fa-arrow-up"></i>';
+	if (!isset($options['icons']['close'])) $options['icons']['move-down'] = '<i class="fa fa-arrow-down"></i>';
 
         $this->cropperOptions = $options;
     }
