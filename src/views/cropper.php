1664846690108948a3600374a802ff60fc3f4ed1
Ercan Bilgin <bilginnet@hotmail.com>
2017-08-28 18:03:50 +0300

    Update cropper.php

--- src/views/cropper.php
+++ src/views/cropper.php
@@ -39,7 +39,7 @@ if ($label !== false) {
 
 <div class="cropper-container clearfix">
 
-    <input type="text" id="<?= $inputOptions['id'] ?>" name="<?=  $inputOptions['name'] ?>" title="" style="width: 0; height: 0; border: none;">
+    <input type="text" id="<?= $inputOptions['id'] ?>" name="<?=  $inputOptions['name'] ?>" title="" style="width: 1px; height: 1px; border: none;">
 
     <?= Html::button($browseLabel, [
         'class' => 'btn btn-primary',
